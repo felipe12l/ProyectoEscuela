3229c9c2ead8429e2b4d6350160ef2a86ebe24fc
Diego Combariza <diego.combariza@uptc.edu.co>
2023-08-03 00:42:27 -0500

    Suggestion commit withg suggestion documentation

--- ProyectoEscuela/src/co/edu/uptc/model/Suggestion.java
+++ ProyectoEscuela/src/co/edu/uptc/model/Suggestion.java
@@ -1,11 +1,26 @@
 package co.edu.uptc.model;
 
-import co.edu.uptc.model.persontypes.Student;
+
+/**
+ * The Suggestion class represents a suggestion made by a student. It contains information about the date,
+ * the content of the suggestion, and the student who made the suggestion.
+ * This class provides methods to get and set the date, content, and associated student account.
+ *
+ * @author Diego Combariza
+ * @version 1.0.0
+ */
 
 public class Suggestion {
     private String date;
     private String content;
     private Account account;
+    /**
+     * Constructs a new Suggestion object with the provided date, content, and associated student account.
+     *
+     * @param date    The date when the suggestion was made.
+     * @param content The content of the suggestion.
+     * @param account The Account object representing the student who made the suggestion.
+     */
 
     public Suggestion(String date, String content, Account account) {
         this.date = date;
@@ -35,7 +50,6 @@ public class Suggestion {
     public Account getAccount() {
         return account;
     }
-
     public void setAccount(Account account) {
         this.account = account;
     }
