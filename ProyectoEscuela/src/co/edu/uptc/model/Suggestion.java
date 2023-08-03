8ecda08bae59b58366175bdeb60720ad226ddda7
Diego Combariza <diego.combariza@uptc.edu.co>
2023-08-03 00:35:28 -0500

    Suggestion commit

--- ProyectoEscuela/src/co/edu/uptc/model/Suggestion.java
+++ ProyectoEscuela/src/co/edu/uptc/model/Suggestion.java
@@ -1,4 +1,51 @@
 package co.edu.uptc.model;
 
+import co.edu.uptc.model.persontypes.Student;
+
 public class Suggestion {
+    private String date;
+    private String content;
+    private Account account;
+
+    public Suggestion(String date, String content, Account account) {
+        this.date = date;
+        this.content = content;
+        this.account = account;
+    }
+
+    public Suggestion() {
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    @Override
+    public String toString() {
+        return "Suggestion{" +
+                "date='" + date + '\'' +
+                ", content='" + content + '\'' +
+                ", student=" + account +
+                '}';
+    }
 }
